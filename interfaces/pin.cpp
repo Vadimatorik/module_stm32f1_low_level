d3357f204092b32b23198871e5b6cd9094575503
Vadim Deryabkin <deryabkinda@mail.ru>
2017-11-21 10:42:44 +0700

    Допилил необходимые методы для PIN.

--- interfaces/pin.cpp
+++ interfaces/pin.cpp
@@ -1,5 +1,8 @@
 #include "pin.h"
 
+//**********************************************************************
+// Включение тактирования порта.
+//**********************************************************************
 static void gpio_clk_en (  const GPIO_TypeDef* GPIOx, const uint32_t remap_mode ) {
     if ( remap_mode != 0 ) {
         __HAL_RCC_AFIO_CLK_ENABLE();
@@ -41,6 +44,9 @@ static void gpio_clk_en (  const GPIO_TypeDef* GPIOx, const uint32_t remap_mode 
     }
 }
 
+//**********************************************************************
+// PIN.
+//**********************************************************************
 void pin::init ( void ) const {
     gpio_clk_en( this->cfg->GPIOx, this->cfg->remap_mode );
     HAL_GPIO_Init( this->cfg->GPIOx, ( GPIO_InitTypeDef * )this->cfg );
@@ -78,3 +84,71 @@ bool pin::read ( void ) const {
     return HAL_GPIO_ReadPin ( this->cfg->GPIOx, ( uint16_t )this->cfg->init.Pin );
 }
 
+//**********************************************************************
+// MULTIFUNC.
+//**********************************************************************
+bool pin_multifunc::init ( uint32_t number_cfg ) const {
+    if ( number_cfg >= this->number ) return false;
+    gpio_clk_en( this->cfg[number_cfg].GPIOx, this->cfg[number_cfg].remap_mode );
+    this->reinit( number_cfg );             // Инициализируем
+    return true;
+}
+
+bool pin_multifunc::reinit ( uint32_t number_cfg ) const {
+    if ( number_cfg >= this->number ) return false;
+    HAL_GPIO_Init( cfg[number_cfg].GPIOx, (GPIO_InitTypeDef *) &cfg[number_cfg].init );
+    return true;
+}
+
+void pin_multifunc::set ( void ) const {
+    HAL_GPIO_WritePin ( this->cfg->GPIOx, (uint16_t) this->cfg->init.Pin, GPIO_PIN_SET);
+}
+
+void pin_multifunc::reset ( void ) const {
+    HAL_GPIO_WritePin ( this->cfg->GPIOx, (uint16_t) this->cfg->init.Pin, GPIO_PIN_RESET);
+}
+
+void pin_multifunc::set ( bool state ) const {
+    if ( state ) {
+        this->set();
+    } else {
+        this->reset();
+    }
+}
+
+void pin_multifunc::set ( int state ) const {
+    this->set( static_cast< bool >( state ) );
+}
+
+void pin_multifunc::set ( uint32_t state ) const {
+    this->set( static_cast< bool >( state ) );
+}
+
+void pin_multifunc::toggle ( void ) const {
+    HAL_GPIO_TogglePin( this->cfg->GPIOx, ( uint16_t )this->cfg->init.Pin );
+}
+
+bool pin_multifunc::read ( void ) const {
+    return HAL_GPIO_ReadPin ( this->cfg->GPIOx, ( uint16_t )this->cfg->init.Pin );
+}
+
+//**********************************************************************
+// MULTIFUNC_IT.
+//**********************************************************************
+bool pin_multifunc_it::check_it ( void ) const {
+    return static_cast< bool >( __HAL_GPIO_EXTI_GET_IT( this->cfg->init.Pin ) );
+}
+
+void pin_multifunc_it::clear_it ( void ) const {
+    __HAL_GPIO_EXTI_CLEAR_IT( this->cfg->init.Pin );
+}
+
+//**********************************************************************
+// GLOBAL_PORT.
+//**********************************************************************
+void global_port::reinit_all_ports ( void ) const {
+    for ( uint32_t l = 0; l < this->number_pin; l++ ) {
+        gpio_clk_en( this->cfg[l].GPIOx, this->cfg[l].remap_mode );
+        HAL_GPIO_Init( cfg[l].GPIOx, ( GPIO_InitTypeDef* ) ( &cfg[l].init ) );
+    }
+}
--- interfaces/pin.h
+++ interfaces/pin.h
@@ -9,6 +9,14 @@
 
 #define PIN_COUNT(x) sizeof(x)/sizeof(x[0])
 
+// Ответы от функций reinit_all, reinit_port объекта global_port.
+enum class EC_ANSWER_GP {
+    SUCCESS = 0,    // Переинициализация была успешной.
+    LOOK    = 1     // Порт заблокирован, переинициализация некоторых
+                    // (или всех) выводов невозможна.
+};
+
+
 // Структуры должны быть объявлены в extern "C" области или C файле.
 struct pin_cfg {
     GPIO_TypeDef*               const GPIOx;
@@ -37,13 +45,44 @@ private:
 
 };
 
-class pin_multifunc : public pin, public pin_multifunc_base {
+class pin_multifunc : public pin_multifunc_base {
 public:
-    pin_multifunc( const pin_cfg* const cfg, uint32_t count_cfg ) : pin( cfg ), count_cfg( count_cfg ) {}
-    bool    reinit  ( uint32_t number_cfg )  const;
+    pin_multifunc ( const pin_cfg* const cfg, uint32_t	number ): cfg( cfg ), number( number ) {}
+    bool	init		( uint32_t number_cfg = 0 ) const;	// Перед инициализацией включается тактирование портов.
+                                                            // Вызвать при первой инициализации.
+
+    bool    reinit      ( uint32_t number_cfg = 0 ) const;
+
+    void    set         ( void )   			const;
+    void    reset       ( void )   			const;
+
+    void	set			( bool state )      const;
+    void    set         ( int state )       const;
+    void    set         ( uint32_t state )  const;
+
+    void	toggle		( void )            const;
+    bool	read		( void )            const;
+
+
+protected:
+    const	pin_cfg*    const		cfg;				// Массив структур переинициализации вывода.
+    const 	uint32_t	number;
+};
+
+class pin_multifunc_it : public pin_multifunc, public pin_multifunc_it_base {
+public:
+    bool check_it   (void) const;
+    void clear_it   (void) const;
+};
+
+class global_port : public global_port_base {
+public:
+    global_port( const pin_cfg* const cfg_array, uint32_t number_pin ) : cfg( cfg_array ), number_pin( number_pin ) {}
+    void reinit_all_ports ( void ) const;
 
 private:
-    const uint32_t      count_cfg;
+    const	pin_cfg*    const		cfg;
+    const 	uint32_t	number_pin;
 };
 
 #endif
